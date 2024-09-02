05e12d4abf1f6ee015bb49de4dd3300b47ea760e
ajaydev <ajaydevzzz30@gmail.com>
2024-09-02 12:40:25 +0530

    add app

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import todoSlice  from  '../redux/todoSlice'
+
+
+export default configureStore({
+  reducer: {
+    todos: todoSlice,
+  },
+});
+
